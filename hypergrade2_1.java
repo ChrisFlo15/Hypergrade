4d3738270f53337e9afd2c0333565acb8977df70
ChrisFlo15 <104855466+ChrisFlo15@users.noreply.github.com>
2022-06-17 12:45:22 -0700

    Update hypergrade2_1.java

--- hypergrade2_1.java
+++ hypergrade2_1.java
@@ -1,18 +1,13 @@
 import java.util.Scanner;
 
-public class hypergrade2_1
+public class Question1
 {
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
-        
-        System.out.println("Enter number");
-        int number1 = keyboard.nextInt();
-        
-        System.out.println(number1);
-        
-        int answer=number1+1;
-        
-        System.out.println(answer);
+        System.out.println("Enter an integer");
+        int number = keyboard.nextInt();
+        int answer = number + 1;
+        System.out.println(number + " + 1 = " + answer);
     }
 }
